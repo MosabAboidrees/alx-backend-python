0cd89d3c018bd43bb134ecfb49dd4bd01e86409d
MosabAboidrees <mosab_mahala@yahoo.com>
2024-10-14 17:34:21 +0300

    Task 0. The basics of async fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -20,7 +20,8 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     """
     wait_random(max_delay: int = 10) -> float
-    Asynchronous function that waits for a random delay between 0 and max_delay seconds.
+    Asynchronous function that waits for a random delay
+    between 0 and max_delay seconds.
     Args:
         max_delay (int): The maximum delay in seconds. Default is 10.
     Returns:
