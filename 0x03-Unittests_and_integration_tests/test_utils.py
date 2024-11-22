7c89ae6e7d397202fd9156a80a368efc457d4255
MosabAboidrees <mosab_mahala@yahoo.com>
2024-11-22 17:42:55 +0300

    Task 1. Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -24,10 +24,12 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b"))
-    ])
+    @parameterized.expand(
+        [
+            ({}, ("a",), KeyError),
+            ({"a": 1}, ("a", "b"), KeyError)
+        ]
+    )
     def test_access_nested_map_exception(self, nested_map, path,
                                          expected_output):
         """
